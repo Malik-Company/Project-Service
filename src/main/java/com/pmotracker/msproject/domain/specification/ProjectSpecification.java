511af29f3e4f659b15d4281bc000c2d99cdb0938
Malik Imran <msabir@nisum.com>
2026-06-23 20:21:55 +0500

    test build issue fixing

--- src/main/java/com/pmotracker/msproject/domain/specification/ProjectSpecification.java
+++ src/main/java/com/pmotracker/msproject/domain/specification/ProjectSpecification.java
@@ -9,9 +9,7 @@ package com.pmotracker.msproject.domain.specification;
  */
 
 import com.pmotracker.msproject.application.interfaces.web.rest.v1.criteria.ProjectCriteria;
-import com.pmotracker.msproject.domain.model.BaseModel_;
 import com.pmotracker.msproject.domain.model.Project;
-import com.pmotracker.msproject.domain.model.Project_;
 import com.pmotracker.msproject.infrastructure.common.RecordStatus;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
@@ -29,7 +27,7 @@ public class ProjectSpecification implements Specification<Project> {
     @Override
     public Predicate toPredicate(Root<Project> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
 
-        Predicate predicate = criteriaBuilder.equal(root.get(BaseModel_.recordStatus), RecordStatus.LIVE);
+        Predicate predicate = criteriaBuilder.equal(root.get("recordStatus"), RecordStatus.LIVE);
 
         if (criteria == null) {
             return predicate;
@@ -37,28 +35,28 @@ public class ProjectSpecification implements Specification<Project> {
 
         if (criteria.getUpdatedAt() > 0) {
             predicate = criteriaBuilder.and(predicate,
-                    criteriaBuilder.greaterThan(root.get(BaseModel_.updatedAt), criteria.getUpdatedAt()));
+                    criteriaBuilder.greaterThan(root.<Long>get("updatedAt"), criteria.getUpdatedAt()));
         }
 
         if (criteria.getRecordStatus() != null) {
             predicate = criteriaBuilder.and(predicate,
-                    criteriaBuilder.equal(root.get(BaseModel_.recordStatus), criteria.getRecordStatus()));
+                    criteriaBuilder.equal(root.get("recordStatus"), criteria.getRecordStatus()));
         }
 
         if (StringUtils.isNotBlank(criteria.getName())) {
             predicate = criteriaBuilder.and(predicate,
-                    criteriaBuilder.like(criteriaBuilder.lower(root.get(Project_.title)),
+                    criteriaBuilder.like(criteriaBuilder.lower(root.<String>get("title")),
                             String.format("%%%s%%", criteria.getName().toLowerCase())));
         }
 
         if (criteria.getStatus() != null) {
             predicate = criteriaBuilder.and(predicate,
-                    criteriaBuilder.equal(root.get(Project_.status), criteria.getStatus()));
+                    criteriaBuilder.equal(root.get("status"), criteria.getStatus()));
         }
 
         if (StringUtils.isNotBlank(criteria.getLocation())) {
             predicate = criteriaBuilder.and(predicate,
-                    criteriaBuilder.equal(root.get(Project_.cityCode), criteria.getLocation()));
+                    criteriaBuilder.equal(root.get("cityCode"), criteria.getLocation()));
         }
 
         return predicate;
